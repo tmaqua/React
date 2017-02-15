559e8c8cdc1c6a08178e2d708a640856c1cc885e
murakami <tmaqua007@gmail.com>
2017-02-16 00:02:45 +0900

    Deleted hello_world controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @hello_world_props = { name: "Stranger" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'hello_world', to: 'hello_world#index'
   mount API => '/'
 
   ## for LandingPage
