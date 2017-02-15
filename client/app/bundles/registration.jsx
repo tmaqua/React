f612860e1a24bad94590c06c85a9b4d9d72257e8
murakami <tmaqua007@gmail.com>
2017-02-16 02:31:53 +0900

    show gakufes twitter

--- client/app/bundles/registration.jsx
+++ client/app/bundles/registration.jsx
@@ -1,8 +1,9 @@
 import ReactOnRails from 'react-on-rails';
-
-import HelloWorld from '../components/HelloWorld';
+import HelloWorld from './HelloWorld/components/HelloWorld';
+import SampleCard from './SampleCard/components/SampleCard';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorld,
+  SampleCard
 });
